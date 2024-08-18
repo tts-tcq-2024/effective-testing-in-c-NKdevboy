b979ee4546bcc42438049365a4346f1ea5f2b7d0
Nithishkumar R (MS/ESG1-VM) <R.Nithishkumar@in.bosch.com>
2024-08-18 10:37:24 +0530

    testing the test case

--- alerter.c
+++ alerter.c
@@ -3,17 +3,18 @@
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+typedef int (*networkAlertStub)(float);
+networkAlertStub GlobalNetworkAlertMork;
+float CurrentCelcius=0;
+
+int networkAlertmock(float celcius) {
+    CurrentCelcius = celcius;
+    return 500;
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = GlobalNetworkAlertMork(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -24,8 +25,13 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
+    GlobalNetworkAlertMork = &networkAlertmock;
     alertInCelcius(400.5);
+    assert(CurrentCelcius == 204.72222); 
     alertInCelcius(303.6);
+    assert(CurrentCelcius == 150.88889);
+    assert(alertFailureCount == 2);
+    
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
--- misaligned.c
+++ misaligned.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+typedef void (*printWrapType)(int,int,const char *,const char *);
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+printWrapType printWrapPtr;
+
+void Printerinterface(int i,int j,const char *majorColor_l,const char *minorColor_l)
+{
+    printf("%d | %s | %s\n", i * 5 + j, majorColor_l, minorColor_l);
+}
+
+void PrinterinterfaceMock(int i,int j,const char *majorColor_l,const char *minorColor_l)
+{
+    assert(strcmp(majorColor_l, majorColor[i])==0);
+    assert(strcmp(minorColor_l, minorColor[j])==0);
+}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printWrapPtr(i,j,majorColor[i], minorColor[i]);
         }
     }
     return i * j;
 }
 
 int main() {
+    // production code 
+    // printWrapPtr = &Printerinterface;
+
+    //Test code
+    printWrapPtr = &PrinterinterfaceMock;
+
     int result = printColorMap();
     assert(result == 25);
     printf("All is well (maybe!)\n");
